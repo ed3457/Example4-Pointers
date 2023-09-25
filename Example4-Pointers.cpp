b7d60c007dabbcc1a0105507feb3a6ba76c15576
Kamel R <kamel.r@wayne.edu>
2023-09-25 18:32:03 -0400

    Add project files.

---
+++ Example4-Pointers.cpp
@@ -0,0 +1,47 @@
+// Example4-Pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+   	int x = 5; 
+
+	int* p; // pointer p of type int
+
+	p = &x;// store the address of x inside the pointer p
+
+	//cout << x << endl;
+
+	//cout << p << endl; // pointer address value 
+
+	//cout << *p << endl; // the value that the pointer is pointing to
+
+	x = 12; 
+
+	*p = 15;
+
+	cout << x << endl;
+	cout << &x << endl; 
+
+	int ar[10];
+
+	int size;
+	cout << "Enter the size of the array:" << endl;
+	cin >> size; 
+	int* pAr = new int[size];
+
+	for (int i = 0;i < size;i++)
+	{
+		cout << "Enter element " ;
+		cout << i+1 << endl;
+		cin >> pAr[i]; 
+	}
+
+
+
+
+
+}
+
